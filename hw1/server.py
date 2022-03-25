da1525f6ca63e288138cb56d5b02d9257be6c4e8
Hyungsuk Kang <hskang9@gmail.com>
2022-03-25 23:49:09 +0900

    add view functions in submission

--- hw1/server.py
+++ hw1/server.py
@@ -82,7 +82,56 @@ def check_cookie(user):
     return diff <= 120
 
 """
-socket related functions(handler)
+view related functions
+"""
+
+def edit_user_storage_html(user):
+    # get list of files in files directory
+    filelist = get_file_list(f'{user}')
+    # edit storage.html
+    htmlFS = open('html' + '/storage.html')
+    content = htmlFS.read()
+    htmlFS.close()
+
+    # replace user1 with received username
+    pattern = re.compile(r"user1")
+    change_user = pattern.sub(user, content)
+    
+    # find ul in storage.html with finding ul pattern
+    pattern = re.compile(r"\<ul[^\>]*\>\s*\<\/ul\>")
+    # replace it with mad html
+    html = "<ul>"
+    for i in filelist:
+        list_element = f"<li>{i} <a href=\"/{user}/{i}\" download><button>Download</button></a> <a href=\"/delete/{user}/{i}\"><button>Delete</button></a> </li>"
+        html+=list_element
+    html+="</ul>"
+    result = pattern.sub(html, change_user)
+    return result
+
+def edit_user_cookie_html(user):
+    # get list of files in files directory
+    filelist = get_file_list(f'{user}')
+    # edit storage.html
+    htmlFS = open('html' + '/cookie.html')
+    content = htmlFS.read()
+    htmlFS.close()
+
+    # replace user1 with received username
+    pattern = re.compile(r"user1")
+    change_user = pattern.sub(user, content)
+    
+    # find ul in storage.html with finding ul pattern
+    pattern = re.compile(r"27")
+    cookieFS = open('cookies/' + user)
+    content = cookieFS.read()
+    cookieFS.close()
+   
+    # replace time in html
+    result = pattern.sub(str(time() - float(content)), change_user)
+    return result
+
+"""
+socket related functions(controller)
 """
 
 def init_socket():
